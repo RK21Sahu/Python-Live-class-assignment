d37349f6b62ea04307d0d70f516576f691071b73
Radhakanta Sahu <97846844+RK21Sahu@users.noreply.github.com>
2022-02-26 02:33:49 +0530

    mainSQLite3

---
+++ mainSQLite3.py
@@ -0,0 +1,14 @@
+
+
+from SQLiteprog import SQLoperation
+
+
+
+obj= SQLoperation()
+
+#obj.droptable()
+#obj.sqliteoperation("vocab.kos.txt","vocab.enron.txt","vocab.nips.txt","vocab.nytimes.txt","vocab.pubmed.txt")
+
+obj.countdup()
+
+
